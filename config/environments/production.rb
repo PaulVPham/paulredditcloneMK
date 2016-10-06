5cee8d9b87c719b5d02a7fdfcdf10709f905901d
PaulVPham <p.pham204@gmail.com>
2016-10-05 22:22:08 -0700

    reverting too lazy to reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #need to enable config for prod
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  #config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
 end
